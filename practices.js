3804c1693c6a053f04fe4c115faa25aecb3ec161
nathira99 <nathirafarveen99@gmail.com>
2025-04-19 11:54:40 +0530

    first commit

---
+++ practices.js
@@ -0,0 +1,75 @@
+// // Exercise: 1 Given a number check whether the number is positive or negative.
+
+// let num = 15;
+
+// if (num > 0) {
+//   console.log("The number is positive!");
+// } else {
+//   console.log("The number is negative!");
+// }
+
+// // Using functions
+
+// function checkNum(num){
+//     if (num > 0) {
+//         console.log("The number is positive!");
+//       } else {
+//         console.log("The number is negative!");
+//       }
+// }
+
+// checkNum(15);
+// checkNum(-2);
+// checkNum(0);
+
+// // Exercise: 2 Given a number check whether the number is even number or odd number.
+
+// let num1 = 154;
+
+// if (num1 % 2 == 0){
+//     console.log("Number is EVEN!")
+// }
+// else{
+//     console.log("Number is ODD!")
+// }
+
+// Task: Given two numbers, add the numbers and multiply the numbers and print the results.
+
+// //array
+
+// let num2 = [9, 12, 3, 4, 5];
+
+// //push: to add an element at the end of the array
+// num2.push(6);
+
+// //unshift: to add an element at the beginning of the array
+// num2.unshift(1);
+
+// //pop: to remove the last element of the array
+// num2.pop();
+
+// //shift: to remove the first element of the array
+// num2.shift();
+// console.log(num2);
+
+// //splice: to insert an element without deleting any element
+// // num2.splice(2, 0, 25); // insert 25 at index 2
+// num2.splice(2, 0, 25);
+// console.log(num2);
+
+// //splice: to remove an element at a specific index
+// // num2.splice(3, 3); // remove 3 elements starting from index 3
+// num2.splice(3, 3);
+// console.log(num2);
+
+// //splice: to replace an element at a specific index
+// // num2.splice(1, 1, 100); // replace element at index 1 with 100
+// num2.splice(1, 1, 100);
+// console.log(num2);
+
+// for(let i = 0; i <= 2; i++){
+//     num2.splice(i, 1);
+//     console.log(num2);
+// }
+
+let num3 = [1, 2, 3, 4, 5];
